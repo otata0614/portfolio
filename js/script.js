66e8bfc737a13fa5bc075daa2d5e274fc9cd7392
otata0614 <89198907+otata0614@users.noreply.github.com>
2021-08-22 21:06:04 +0900

    Add files via upload
    img修正

--- js/script.js
+++ js/script.js
@@ -1,15 +1,63 @@
-
 //=====================================================
 // スムーススクロール
 //=====================================================
 
-$(function(){
-  $('a[href^="#"]').click(function(){
+$(function () {
+  $('a[href^="#"]').click(function () {
     let speed = 500;
-    let href= $(this).attr("href");
+    let href = $(this).attr("href");
     let target = $(href == "#" || href == "" ? 'html' : href);
     let position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
     return false;
   });
 });
+
+//=====================================================
+// swiper
+//=====================================================
+
+let mySwiper = new Swiper('.swiper-container', {
+  // オプションパラメータ(一部のみ抜粋)
+  loop: true, // 最後のスライドまで到達した場合、最初に戻らずに続けてスライド可能にするか。
+  speed: 600, // スライドが切り替わるトランジション時間(ミリ秒)。
+  slidesPerView: 1, // 何枚のスライドを表示するか
+  spaceBetween: 10, // スライド間の余白サイズ(ピクセル)
+  direction: 'horizontal', // スライド方向。 'horizontal'(水平) か 'vertical'(垂直)。effectオプションが 'slide' 以外は無効。
+  effect: 'slide', // "slide", "fade"(フェード), "cube"(キューブ回転), "coverflow"(カバーフロー) または "flip"(平面回転)
+
+  // スライダーの自動再生
+  // autoplay: true 　のみなら既定値での自動再生
+  autoplay: {
+    delay: 3000, // スライドが切り替わるまでの表示時間(ミリ秒)
+    stopOnLast: false, // 最後のスライドまで表示されたら自動再生を中止するか
+    disableOnInteraction: true // ユーザーのスワイプ操作を検出したら自動再生を中止するか
+  },
+  //ページネーション表示の設定
+  pagination: {
+    el: '.swiper-pagination', //ページネーションの要素
+    type: 'bullets', //ページネーションの種類
+    clickable: true, //クリックに反応させる
+  },
+
+  //ナビゲーションボタン（矢印）表示の設定
+  navigation: {
+    nextEl: '.swiper-button-next', //「次へボタン」要素の指定
+    prevEl: '.swiper-button-prev', //「前へボタン」要素の指定
+  },
+})
+
+
+//=====================================================
+// パララックス
+//=====================================================
+if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0) {
+    // スマートフォン向けの記述
+} else if (navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+    // タブレット向けの記述
+} else {
+    // PC向けの記述
+    luxy.init();
+}
